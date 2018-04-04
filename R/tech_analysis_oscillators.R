506e077eff6e9794d650750d8d271474517d20a7
Ghazy Mahjub <traderghazy@gmail.com>
2018-04-03 19:50:53 -0500

    Committing last of tech analysis code, just missing acceleration oscillator code.

--- R/tech_analysis_oscillators.R
+++ R/tech_analysis_oscillators.R
@@ -54,9 +54,84 @@ calc_accumulation_distribution_line<-function(ticker, window_size = 21){
   return (return_series)  
 }
 
-calc_accumulation_distribution_oscillator<-function(ticker, maSlow = list("EMA", n =10), maFast = list("EMA", n=3), percent=FALSE){
+#' calc_accumulation_distribution_oscillator
+#' 
+#' This is also known as a Daily Raw Figure. The maximum value is 100 and the minimum value is 0.
+#' The value is normalied to the range. The maximum value is reached when the market opens trading at
+#' the low and closes trading at the high. When the market opens at the high and closes at the low,
+#' the minimum value of 0 is reached. The day to day use of this indicator may require some sort of 
+#' smoothing as it is very erratic. Week to week or montly periodicity may not, is it up to the user
+#' to figure this out. Smoothing this value of out and drawing two lines, one at the top representing
+#' overbought conditions and one at the bottom representing over-sold conditions.
+#'
+#' @param smoothing TRUE/FALSE, use smoothing or not.
+#' @param maType default EMA, can be any one of WMA, SMA, EMA, etc... help(EMA) for list.
+#' @param window_size default is 6, representing a ~ 30% smoothing constant.
+#' @param ticker ticker string.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_accumulation_distribution_oscillator<-function(ticker, smoothing = FALSE, maType = EMA, window_size = 6 ){
   xts_obj<-get(ticker)
-  hlc_px_series<-HLC(xts_obj)
-  volume_series<-Vo(xts_obj)
-  chaikinOscill
+  ado_num<- Hi(xts_obj) - Op(xts_obj) + Cl(xts_obj) - Lo(xts_obj)
+  ado_den<-2*(Hi(xts_obj) - Lo(xts_obj))
+  ado<-100.0*ado_num/ado_den
+  colnames(ado) <- c(paste(ticker, "ADO", sep = "."))
+  if (smoothing){
+    smoothed_ado<-do.call(maType, list(ado, n=window_size))
+    ado<-merge(ado, smoothed_ado)
+    colnames(ado)<-c(names(ado)[1], paste(ticker, window_size, "Per.EMA.ADO.", sep = "."))
+  }
+  return(ado)
+}
+
+#' calc_momentum_oscillator
+#'
+#' Simply, the ratio of some price (Op, Hi, Lo, Cl) at time t, to that same
+#' price type at time t - time_lag. The time_lag here is some amount of time
+#' in the rear, and it is in number of periods. Therefore, exactly how much
+#' time it is depends on the periodicity of the inputted data. There is no
+#' changing of periodicity going on here.
+#'
+#' @param time_lag 
+#' @param ticker
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_momentum_oscillator<-function(ticker, px_type = "A", time_lag=5){
+  xts_obj<-get(ticker)
+  px_series <- Ad(xts_obj)
+  if (px_type == "O"){
+    px_series<- Op(xts_obj)
+  } else if (px_type == "H"){
+    px_series <- Hi(xts_obj)
+  } else if (px_type == "L"){
+    px_series <- Lo(xts_obj)
+  } else if (px_type == "C"){
+    px_series <- Cl(xts_obj)
+  }
+  mom_rat<-px_series/lag.xts(px_series, k = time_lag)
+  colnames(mom_rat)<-c(paste(ticker, time_lag, "PerLagMomOsc", sep = "." ))
+  adj_px_series<-Ad(xts_obj)
+  mom_rat<-merge(mom_rat, adj_px_series)
+  return(mom_rat)
+}
+
+calc_mom_accel_oscillator<-function(ticker, px_type = "A", time_lag = 5){
+  xts_obj<-get(ticker)
+  px_series<-Ad(xts_obj)
+  if (px_type == "O"){
+    px_series<-Op(xts_obj)
+  } else if (px_type == "H"){
+    px_series <- Hi(xts_obj)
+  } else if (px_type == "L"){
+    px_series <- Lo(xts_obj)
+  } else if (px_type == "C"){
+    px_series <- Cl(xts_obj)
+  }
+  
 }
