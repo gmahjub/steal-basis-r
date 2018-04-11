717aca56815f9c98a6f32c0f86c3615a03e4d131
Ghazy Mahjub <gmahjub@yahoo.com>
2018-04-11 18:28:32 -0500

    Small upates to alphavantager ticker pull code.

--- R/write_intraday_av.R
+++ R/write_intraday_av.R
@@ -12,6 +12,10 @@
 #' @examples
 write_intraday_av <- function(ticker, data_obj, path_to_ticker_dir, column_names = NULL){
   ticker_csv_file <- paste(path_to_ticker_dir, ticker, ".csv", sep = "")
-  data_obj<-data_obj %>% setNames(., c("BarTimeStamp", "Open", "High", "Low", "Close", "Volume"))
+  if (!is.null(column_names)){
+    data_obj<-data_obj %>% setNames(., column_names)
+  } else {
+    data_obj<-data_obj %>% setNames(., c(" ", "BarTimeStamp", "Open", "High", "Low", "Close", "Volume"))
+  }
   write.csv(data_obj, file = ticker_csv_file)
 }
